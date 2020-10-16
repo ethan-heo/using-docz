75f0d9b2e399d78166d7a609ae98a3639ba3dba1
uchang heo <ucheo@rsupport.com>
2020-10-16 13:33:26 +0900

    update docs files

--- doczrc.js
+++ doczrc.js
@@ -2,11 +2,5 @@ export default {
     typescript: true,
     title: "프로젝트 스펙 문서입니다.",
     description: "설명입니다",
-    menu: [
-        "Intro",
-        {
-            name: "Concept",
-            menu: ["Component", "Container", "Store", "Module", "Utility"]
-        }
-    ]
+    menu: ["Intro", "Folder Structure"]
 }
